02449dcfc395bdb8e33ce6ad4d4e230fab950526
jing <jinghaoyao@gmail.com>
2019-07-16 23:57:16 -0400

    Initial commit
    first github commit

---
+++ README.txt
@@ -0,0 +1,26 @@
+The dataset used in this project came from Kaggle (https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge) 
+and originally from:
+"Challenges in Representation Learning: A report on three machine learning 
+contests." I Goodfellow, D Erhan, PL Carrier, A Courville, 
+M Mirza, B
+Hamner, W Cukierski, Y Tang, DH Lee, Y Zhou, C Ramaiah, F Feng, R Li,
+ X Wang, D Athanasakis, J Shawe-Taylor, M Milakov, J Park, 
+R Ionescu,
+ M Popescu, C Grozea, J Bergstra, J Xie, L Romaszko, B Xu, Z Chuang, and
+Y. Bengio. arXiv 2013. 
+
+See fer2013.bib for a bibtex entry.
+
+
+Final FER.py uses kaggle_emotions_model.h5 which has significantly higher detection than emotions_model.h5
+trial.py uses emotions_model.h5
+
+open-cv webcam capture: https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
+open-cv face detection: https://docs.opencv.org/3.4.1/d7/d8b/tutorial_py_face_detection.html
+
+Necessary Installations:
+Tensorflow: https://pypi.org/project/tensorflow/
+Keras: https://keras.io/#installation
+open-cv: https://pypi.org/project/opencv-python/
+numpy
+matplotlib
---
+++ trial.py
@@ -0,0 +1,79 @@
+import tensorflow as tf
+import keras
+from keras.models import load_model
+from keras.preprocessing import image
+
+import cv2
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import operator
+
+path = os.path.dirname(os.path.abspath(__file__))
+print(path)
+model = load_model(path + "/emotions_model.h5")
+
+#emotion predictions
+def emotion_analysis(emotions):
+    objects = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+
+    for ob in range(len(objects[:6])):
+        print(objects[ob] + ' percentage: ' + str(emotions[ob]))
+    index,max_val = max(enumerate(emotions[:6]), key=operator.itemgetter(1))
+    max_percent = round(max_val*100,1)
+    return objects[index] + ' ' + str(max_percent) + '%'
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
+#eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    #flip frame for mirror like effect
+    frame = cv2.flip(frame,1)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # draw faces rectange
+    faces = face_cascade.detectMultiScale(gray,scaleFactor=1.5,minNeighbors=5)
+
+    for (x,y,w,h) in faces:
+        roi_gray = gray[y:y+h+10, x:x+w]
+        roi_color = frame[y:y+h+10, x:x+w]
+        #resize frame
+        test_frame = cv2.resize(roi_gray,(48,48), interpolation = cv2.INTER_AREA)
+
+        #pass through neural network
+        test = image.img_to_array(test_frame)
+        test = np.expand_dims(test, axis = 0)
+        test /= 255
+        custom = model.predict(test)
+        
+        emotion = emotion_analysis(custom[0])
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_color = (255,0,0)
+
+        color = (255,0,0) #BGR 0-255
+        stroke = 2
+        end_cord_x = x+w
+        end_cord_y = y+h+10
+        cv2.rectangle(frame, (x,y), (end_cord_x,end_cord_y),color,stroke)
+        cv2.putText(frame,emotion,(x-10,y-10), font, 0.75, font_color, 1, cv2.LINE_AA)
+
+        #eyes = eye_cascade.detectMultiScale(roi_gray)
+        #for (ex, ey, ew, eh) in eyes:
+            #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+#When everything is done, release the capture
+cap.release()
+cv2.destroyAllWindows()
